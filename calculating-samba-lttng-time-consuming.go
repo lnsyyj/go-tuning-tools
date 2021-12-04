db6d5beafb9cc65a1dcbf9a089fde98d59d76681
JiangYu <lnsyyj@hotmail.com>
2021-12-05 01:12:31 +0800

    print result
    Signed-off-by: JiangYu <lnsyyj@hotmail.com>

--- calculating-samba-lttng-time-consuming.go
+++ calculating-samba-lttng-time-consuming.go
@@ -186,13 +186,17 @@ func analyze_the_result() {
 func lttng_result_print() {
 	fmt.Println("==========================")
 	for _, v := range result_map {
-
 		lttng_reslut_time :=  fmt.Sprintf("%s : %d : %f", v.call_name, v.call_number, v.call_time_sum)
-		println(lttng_reslut_time)
-		lttng_reslut_time = fmt.Sprintf("%s : %d : %f", v.call_name, v.call_number, v.call_time_sum/1000000000)
 		println(lttng_reslut_time)
 		//fmt.Println(v.call_name, v.call_number, v.call_time_sum)
 	}
+	fmt.Println("==========================")
+	for _, v := range result_map {
+		lttng_reslut_time := fmt.Sprintf("%s : %d : %f", v.call_name, v.call_number, v.call_time_sum/1000000000)
+		println(lttng_reslut_time)
+	}
+
+
 }
 
 func main() {
